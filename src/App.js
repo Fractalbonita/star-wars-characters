f9d580ebc46f7506b159408bf7a60789c32b6d0f
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-10-26 12:53:55 +0100

    Import starwarscharacter component into app component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import React from 'react';
+import StarWarsCharacters from './StarWarsCharacters';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <StarWarsCharacters />
+    </div>
+  );
 }
 
 export default App;
