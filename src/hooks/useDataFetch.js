0bb7217a7062f9920025bee34e40d9d65b7b03dc
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-10-26 17:45:18 +0100

    Refactor usedatafetch hook

--- src/hooks/useDataFetch.js
+++ src/hooks/useDataFetch.js
@@ -1,5 +1,27 @@
 import { useEffect, useState } from 'react';
 
+const getData = async (endpoint) => {
+  const data = localStorage.getItem(endpoint);
+  if (data) {
+    return Promise.resolve(JSON.parse(data));
+  }
+  const baseURL = 'https://swapi.dev/api';
+  let nextPage = baseURL + '/' + endpoint + '/';
+  let result = [];
+  try {
+    do {
+      const response = await fetch(nextPage);
+      const data = await response.json();
+      result = result.concat(data.results);
+      nextPage = data.next;
+    } while (nextPage);
+  } catch (error) {
+    console.log(error);
+  }
+  localStorage.setItem(endpoint, JSON.stringify(result));
+  return result;
+};
+
 export function useDataFetch() {
   const [people, setPeople] = useState([]);
   const [films, setFilms] = useState([]);
@@ -28,27 +50,5 @@ export function useDataFetch() {
     });
   }, []);
 
-  const getData = async (endpoint) => {
-    const data = localStorage.getItem(endpoint);
-    if (data) {
-      return Promise.resolve(JSON.parse(data));
-    }
-    const baseURL = 'https://swapi.dev/api';
-    let nextPage = baseURL + '/' + endpoint + '/';
-    let result = [];
-    try {
-      do {
-        const response = await fetch(nextPage);
-        const data = await response.json();
-        result = result.concat(data.results);
-        nextPage = data.next;
-      } while (nextPage);
-    } catch (error) {
-      console.log(error);
-    }
-    localStorage.setItem(endpoint, JSON.stringify(result));
-    return result;
-  };
-
-  return [people, films, planets, species, starships, vehicles, isLoading];
+  return { people, films, planets, species, starships, vehicles, isLoading };
 }
